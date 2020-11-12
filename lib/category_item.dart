4d521ff458a393256cfb84153a759edf6cbecaa0
Tomasz Budny <tomasz.budny.pro@gmail.com>
2020-11-12 22:15:15 +0100

    style: set app theme

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 
 class CategoryItem extends StatelessWidget {
-  final String tital;
+  final String title;
   final Color color;
 
-  CategoryItem(this.tital, this.color);
+  CategoryItem(this.title, this.color);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(15),
-      child: Text(tital),
+      child: Text(
+        title,
+        style: Theme.of(context).textTheme.headline6,
+      ),
       decoration: BoxDecoration(
         gradient: LinearGradient(
           colors: [
